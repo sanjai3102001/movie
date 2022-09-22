428f1e240b1e25ef20c6ff78c8dc6be2db095a62
sanjai3102001 <98545323+sanjai3102001@users.noreply.github.com>
2022-09-22 14:39:08 +0530

    rectifird accedently pulled the comment changes

--- functions/get.go
+++ functions/get.go
@@ -17,12 +17,13 @@ type movie struct {
 }
 
 // this is a Getmovies function
-// func Pullm(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "application/json")
-// 	var movies []movie
-// 	db.Find(&movies)
-// 	json.NewEncoder(w).Encode(movies)
-}
+//
+//	func (w http.ResponseWriter, r *http.Request) {
+//		w.Header().Set("Content-Type", "application/json")
+//		var movies []movie
+//		db.Find(&movies)
+//		json.NewEncoder(w).Encode(movies)
+//	}
 func Getmovieid(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var movies []movie
