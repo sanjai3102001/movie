fb55ff9c5dcfe54b442ef26f430ea395e99dc7b9
sanjai3102001 <98545323+sanjai3102001@users.noreply.github.com>
2022-09-22 14:09:23 +0530

    movie_api
    this is the movie api to be integrated with sonar cloud

---
+++ functions/get.go
@@ -0,0 +1,31 @@
+package function
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+type movie struct {
+	gorm.Model
+	MovieId   string `json:"movieid"`
+	MovieName string `json:"moviename"`
+	Isactive  bool   `json:"Isactive"`
+}
+
+//this is a get function
+func Getmovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movies []movie
+	db.Find(&movies)
+	json.NewEncoder(w).Encode(movies)
+}
+func Getmovieid(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movies []movie
+	db.Find(&movies)
+	json.NewEncoder(w).Encode(movies)
+}
