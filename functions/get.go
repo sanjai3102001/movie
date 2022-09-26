19fb466b1a7f2cfe24c927436fc25e48056cd578
sanjai3102001 <98545323+sanjai3102001@users.noreply.github.com>
2022-09-26 10:04:31 +0530

    github workflow configuration

--- functions/get.go
+++ functions/get.go
@@ -19,9 +19,9 @@ type movie struct {
 // this is a Getmovies function
 func Pullm(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var movies []movie
-	db.Find(&movies)
-	json.NewEncoder(w).Encode(movies)
+	var moviess []movie
+	db.Find(&moviess)
+	json.NewEncoder(w).Encode(moviess)
 }
 func Getmovieid(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
